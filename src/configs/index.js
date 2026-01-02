4c14cc4c46437b8ee0ff38f0c9cc47b2b8132da7
Dharmik Kevadiya <dharmikkevadiya1552@gmail.com>
2026-01-02 23:53:10 +0530

    S3 Flow implemented

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,9 +2,7 @@ import 'dotenv/config';
 
 export const MONGODB_URL = process.env.MONGODB_URL;
 export const JWT_SECRET = process.env.JWT_SECRET;
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const PORT = process.env.PORT;
-export const CLOUDINARY_URL = process.env.CLOUDINARY_URL;
 // AWS / S3
 export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
 export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
--- src/utils/fileStorageImageKit.js
+++ src/utils/fileStorageImageKit.js
@@ -1,39 +1,39 @@
-import ImageKit from 'imagekit';
-import path from 'path';
+// import ImageKit from 'imagekit';
+// import path from 'path';
 
-const imagekit = new ImageKit({
-  publicKey:
-    process.env.IMAGEKIT_PUBLIC_KEY || 'public_fA3jJ5J1Fxzpg/hUoE1zXGt/oSU=',
-  privateKey:
-    process.env.IMAGEKIT_PRIVATE_KEY || 'private_IIIsHU6xrOl6Ga2VQXZtHy8k77o=',
-  urlEndpoint:
-    process.env.IMAGEKIT_URL_ENDPOINT || 'https://ik.imagekit.io/emshrms',
-});
+// const imagekit = new ImageKit({
+//   publicKey:
+//     process.env.IMAGEKIT_PUBLIC_KEY || 'public_fA3jJ5J1Fxzpg/hUoE1zXGt/oSU=',
+//   privateKey:
+//     process.env.IMAGEKIT_PRIVATE_KEY || 'private_IIIsHU6xrOl6Ga2VQXZtHy8k77o=',
+//   urlEndpoint:
+//     process.env.IMAGEKIT_URL_ENDPOINT || 'https://ik.imagekit.io/emshrms',
+// });
 
-/**
- * Upload file to ImageKit
- * @param {Buffer} fileBuffer - File buffer
- * @param {String} fileName - Name for the file
- * @param {String} folder - Folder in ImageKit (optional)
- */
-export const uploadFile = async (file, folder = '/hrms_uploads') => {
-  try {
-    const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+// /**
+//  * Upload file to ImageKit
+//  * @param {Buffer} fileBuffer - File buffer
+//  * @param {String} fileName - Name for the file
+//  * @param {String} folder - Folder in ImageKit (optional)
+//  */
+// export const uploadFile = async (file, folder = '/hrms_uploads') => {
+//   try {
+//     const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
 
-    const extension = path
-      .extname(file.originalname)
-      .toLowerCase()
-      .replace('.', '');
+//     const extension = path
+//       .extname(file.originalname)
+//       .toLowerCase()
+//       .replace('.', '');
 
-    const key = `${uniqueName}.${extension}`;
+//     const key = `${uniqueName}.${extension}`;
 
-    const response = await imagekit.upload({
-      file: file.buffer, // can be buffer, base64, or URL
-      fileName: key,
-      folder,
-    });
-    return response; // contains url, thumbnailUrl, fileId etc.
-  } catch (error) {
-    throw error;
-  }
-};
+//     const response = await imagekit.upload({
+//       file: file.buffer, // can be buffer, base64, or URL
+//       fileName: key,
+//       folder,
+//     });
+//     return response; // contains url, thumbnailUrl, fileId etc.
+//   } catch (error) {
+//     throw error;
+//   }
+// };
